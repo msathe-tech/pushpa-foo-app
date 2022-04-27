24426f47ce0d48e3f5f119ff7fab8725881b6809
abhinavrau <abhinav.rau@gmail.com>
2022-04-27 21:42:41 +0000

    Add Ingress

--- src/main/java/com/google/run/demo/ColorController.java
+++ src/main/java/com/google/run/demo/ColorController.java
@@ -3,19 +3,28 @@ package com.google.run.demo;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
+@Configuration
+@PropertySource("classpath:foo.properties")
 public class ColorController {
     
     HashMap<String, String> map = new HashMap<>();
     
+    @Value("${page.color:BLUE}")
+    private String color;
+
     @GetMapping("/metadata")
     public Map<String, String> metadata() {
         
         if (map.isEmpty()) {
             
-            map.put("color", "PINK");
+            map.put("color", color);
             map.put("Node_Name", System.getenv().get("NODE_NAME"));
             map.put("Host_IP", System.getenv().get("HOST_IP"));
             map.put("Pod_IP", System.getenv().get("POD_IP"));
